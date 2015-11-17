1e84137d0d7896f970ebde39b1f41cafa7064b35
khacsinhcs <khacsinhcs@gmail.com>
2015-11-17 20:51:00 +0700

    init help mod and feedback mod

--- src/main/java/com/muffin/client/MuffinConfiguration.java
+++ src/main/java/com/muffin/client/MuffinConfiguration.java
@@ -1,8 +1,14 @@
 package com.muffin.client;
 
+import com.mvu.core.client.Core;
 import com.mvu.core.client.CoreActivityMapper;
 import com.mvu.core.client.CoreConfiguration;
+import com.mvu.core.client.ModulesManager;
 import com.mvu.core.client.Navigation;
+import com.mvu.core.client.feedback.FeedbackMod;
+import com.mvu.core.client.help.HelpMod;
+import com.mvu.core.shared.Function;
+import com.mvu.core.shared.HasFields;
 
 /**
  * Date: 6/10/14 1:14 PM
@@ -11,6 +17,23 @@ public class MuffinConfiguration extends CoreConfiguration {
   @Override
   public CoreActivityMapper createActivityMapper() {
     return new MuffinActivityMapper();
+  }
+
+  @Override
+  public void initComponents(ModulesManager modulesManager) {
+    super.initComponents(modulesManager);
+    HelpMod.init(new Function<HasFields, Boolean>() {
+      @Override
+      public Boolean eval(HasFields input) {
+        return true;
+      }
+    });
+    FeedbackMod.init(new Function<HasFields, Boolean>() {
+      @Override
+      public Boolean eval(HasFields input) {
+        return true;
+      }
+    });
   }
 
   @Override
